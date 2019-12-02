3beda76908022689e9f366d8df1e1ff3f1df1464
Justin Swiger <justin.swiger@me.com>
2019-12-02 11:35:52 -0500

    wireframe and buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,6 @@ import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <Router>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/1" component={Page}></Route>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
 
 const HomePage = () => {
-  return <HelloWorld />
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  const letterClicked = letter => {
+    console.log(letter)
+  }
+  return (
+    <main>
+      <img src="" alt="snowman" />
+      <section>
+        <ul>
+          <li> _ </li>
+          <li> _ </li>
+          <li> _ </li>
+          <li> _ </li>
+          <li> _ </li>
+          <li> _ </li>
+        </ul>
+      </section>
+      <section>
+        <header>Guessed Letters:</header>
+        <ul>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+          <li>D</li>
+        </ul>
+        <ul className="letter-buttons">
+          {letters.map(letter => {
+            return (
+              <li>
+                <button onClick={() => letterClicked(letter)}>{letter}</button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </main>
+  )
 }
 
 export default HomePage
